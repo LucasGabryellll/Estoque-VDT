b89f7c6163770893ea9958578803fb962ba188bb
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-08-15 23:35:32 -0300

    Database connection

---
+++ src/data-source.ts
@@ -0,0 +1,29 @@
+import "reflect-metadata";
+
+import { DataSource } from "typeorm";
+
+import { Provider } from "./entity/Provider";
+import { Model } from "./entity/Model";
+import { Equipment } from "./entity/Equipment";
+
+export const Connection = async () => {
+  const connection = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "Lu274235Ely5",
+    database: "estoque_vdt",
+    entities: [Provider, Model, Equipment],
+    migrations: [],
+    subscribers: [],
+    synchronize: true,
+    logging: false,
+  });
+
+  try {
+    await connection.initialize();
+  } catch (error) {
+    console.log(error);
+  } 
+}
