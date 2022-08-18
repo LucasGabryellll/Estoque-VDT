2e74f78ba1adb821e6ee4338836063ac8acbfcc9
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-08-18 18:18:15 -0300

    Criando CRUD do Provider

--- src/controller/ProviderController.ts
+++ src/controller/ProviderController.ts
@@ -6,7 +6,7 @@ export const getProviders = async (request: Request, response: Response) => {
   const providers = await Provider.getRepository().find();
 
   return response.json(providers);
-};
+}
 
 export const getProvider = async (request: Request, response: Response) => {
 
@@ -17,57 +17,70 @@ export const getProvider = async (request: Request, response: Response) => {
     }
   });
 
-  return response.json(provider);
+  try {
+    return response.json(provider);
+  } catch (error) {
+    return response.status(400).json({ erro: "Error when fetching  Providers" })
+  }
 };
 
 export const saveProvider = async (request: Request, response: Response) => {
 
-  const provider = await Provider.getRepository().save(request.body);
-
   try {
+    const provider = await Provider.getRepository().save(request.body);
+
     response.json(
       {
         id: provider.id,
-        sucesso: "Fornecedor cadastrado com sucesso"
+        sucess: "Provider registered successfully"
       }
     );
   } catch (error) {
-    return response.status(400).json({ erro: "Verifique os dados e tente novamente" });
+
+    return response.status(400).json({ erro: "Erro saving, check the data and try again!" });
   }
 };
 
 export const updateProvider = async (request: Request, response: Response) => {
   const { id } = request.params;
 
-  const provider = await Provider.getRepository().update(id, request.body);
+  try {
+    const provider = await Provider.getRepository().update(id, request.body);
 
-  if (provider.affected === 1) {
-    const providerUpdated = await Provider.getRepository().findOne({
-      where: {
-        id: parseInt(id)
-      }
-    });
+    if (provider.affected === 1) {
+      const providerUpdated = await Provider.getRepository().findOne({
+        where: {
+          id: parseInt(id)
+        }
+      });
 
-    return response.json(providerUpdated);
+      return response.json(providerUpdated);
+    }
+  } catch (error) {
+
+    return response.status(404).json({ message: 'Provider not found!' });
   }
 
-  return response.status(404).json({ message: 'Provider not found!'})
 }
 
 export const deleteProvider = async (request: Request, response: Response) => {
   const { id } = request.params;
 
-  const provider = await Provider.getRepository().delete(id)
+  try {
+    const provider = await Provider.getRepository().delete(id)
 
-  if (provider.affected === 1) {
-    const providerRemoved = await Provider.getRepository().findOne({
-      where: {
-        id: parseInt(id)
-      }
-    });
+    if (provider.affected === 1) {
+      const providerRemoved = await Provider.getRepository().findOne({
+        where: {
+          id: parseInt(id)
+        }
+      });
 
-    return response.json({ message: "Provider removed" });
+      return response.json({ message: "Provider removed" });
+    }
+  } catch (error) {
+
+    return response.status(404).json({ message: "Provider not found!" });
   }
 
-  return response.status(404).json({ message: "Provider not found!" })
 }
