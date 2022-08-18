01a8af86d1e2063cefffcc8938f6cefb4a32cb3c
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-08-18 18:16:25 -0300

    Adicionando rotas para Model, Equipment e Stock

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,40 @@
-import { Router, Request, Response} from 'express';
+import { Router, Request, Response } from 'express';
 
-import { 
-  getProviders, 
-  saveProvider, 
-  getProvider, 
+import {
+  getProviders,
+  saveProvider,
+  getProvider,
   updateProvider,
   deleteProvider
 } from './controller/ProviderController';
 
+import {
+  getModels,
+  saveModel,
+  getModel,
+  updateModel,
+  deleteModel
+} from './controller/ModelController';
+
+import {
+  getEquipments,
+  getEquipment,
+  saveEquipment,
+  updateEquipment,
+  deleteEquipment
+} from './controller/EquipmentController';
+
+import {
+  getStock,
+  saveStock,
+  deleteStock,
+  updateStock
+} from './controller/Stock_internalController';
+
 const routes = Router();
 
-routes.get('/', (request: Request, response : Response) => {
-  
+routes.get('/', (request: Request, response: Response) => {
+
   return response.json({ message: "Hello World" });
 });
 
@@ -22,6 +45,32 @@ routes.get('/provider', getProviders);
 routes.get('/provider/:id', getProvider);
 routes.post('/provider', saveProvider);
 routes.put('/provider/:id', updateProvider);
-routes.delete('provider/:id', deleteProvider);
+routes.delete('/provider/:id', deleteProvider);
+
+/**
+ * Rotas para a tabela do Model
+ */
+routes.get('/model', getModels);
+routes.get('/model/:id', getModel);
+routes.post('/model', saveModel);
+routes.put('/model/:id', updateModel);
+routes.delete('/model/:id', deleteModel);
+
+/**
+ * Rotas para a tabela de Equipment
+ */
+routes.get('/equipment', getEquipments);
+routes.get('/equipment/:id', getEquipment);
+routes.post('/equipment', saveEquipment);
+routes.put('/equipment/:id', updateEquipment);
+routes.delete('/equipment/:id', deleteEquipment);
+
+/**
+ * 
+ */
+routes.get('/stockIn', getStock);
+routes.post('/stockIn', saveStock);
+routes.put('/stockIn/:id', updateStock);
+routes.delete('/stockIn/:id', deleteStock);
 
 export default routes;
